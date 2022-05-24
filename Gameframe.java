0595e43b89c94a7b343b448be902b4cb543ebf8f
WuChuang12012607 <105142626+WuChuang12012607@users.noreply.github.com>
2022-05-24 16:40:33 +0800

    Add files via upload
    PHOTOS UPLOAD

---
+++ Gameframe.java
@@ -0,0 +1,45 @@
+import javafx.scene.layout.Background;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+
+//首先，我们需要三个部分，Gamepanel，Gameframe ，Staticpanel,Operation以及Gamedata
+public class Gameframe extends JFrame {
+
+
+    Gamepanel gamepanel;
+    Container mainpane;
+    Operation operation;
+    public Staticpanel staticpanel;
+
+    Gameframe(Gamepanel gamepanel, Operation operation) {
+        this.gamepanel = gamepanel;
+        this.operation = operation;
+        mainpane = getLayeredPane();
+        setBounds(100, 50, 600, 900);
+        setTitle("TETRIS");
+        setResizable(false);
+        setLayout(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        ImageIcon imgic=new ImageIcon("小成品.png");
+       JLabel jl=new JLabel(imgic);
+        jl.setBounds(0,0,600,900);
+        getContentPane().add(jl);
+        staticpanel = new Staticpanel(operation);
+        mainpane.add(staticpanel);
+        setGamepanel();
+        setFocusable(true);
+        setLocationRelativeTo(null);
+    }
+    void setGamepanel() {
+        this.setIconImage(new ImageIcon("Tetris.png").getImage());
+        gamepanel = new Gamepanel(operation);
+        mainpane.add(gamepanel);
+    }
+    Gamepanel getGamepanel() {
+        this.setIconImage(new ImageIcon("Tetris.png").getImage());
+        return gamepanel;
+    }
+}
---
+++ continuegamepanel.java
@@ -0,0 +1,90 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Random;
+import java.util.Scanner;
+
+public class continuegamepanel extends JFrame {//我们在这个部分来讨论一下如何把老用户的保存文档输出出来
+    public  static String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    File file;
+    JButton[] bt;
+    JPanel panel ;
+    Scanner scanner;
+    int[][] savemap= new int[10][20];
+    continuegamepanel(){
+        File file = new File(username+".txt");
+        File[] file2 = file.listFiles();
+        panel = new JPanel();panel.setBounds(0,0,250,250);
+        setSize(250, 250);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        panel.setLayout(new GridLayout(21,0,10,10));
+         bt= new JButton[file2.length-1];
+        for(int i=0;i<bt.length;i++){
+            int k=i+1;
+            bt[i]=new JButton(""+k);
+            bt[i].setBounds(10,20*i,180,70);
+            panel.add(bt[i]);
+            int j = i;
+            bt[i].addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    try {String temp = null;
+                        scanner = new Scanner(file2[j+1]);
+                        while (scanner.hasNext()){
+                            temp = scanner.next();
+                        }
+                        scanner.close();
+                        String[] arr = temp.split("");
+                        for(int i= 0;i<arr.length;i++){
+                            System.out.println(arr[i]);
+                        }
+                        int n=0;
+                        for(int i= 0;i<arr.length;i++){
+                            int a1=savemap.length-i%10-1;
+                            int a2=savemap[0].length-1-n;
+                            savemap[a1][a2]= Integer.parseInt(arr[i]) ;
+                           if(i%10==9){ n++;}
+                        }
+                        setVisible(false);
+                        Operation operation = new Operation();
+                        operation.username=username;
+                       for(int i=0;i<savemap.length;i++){
+                           for(int j=0;j<savemap[0].length;j++){
+                               operation.savemap[9-i][19-j]=savemap[i][j];
+                           }
+                       }
+                        Gamepanel gamepanel = new Gamepanel(operation);
+                        Gameframe frame = new Gameframe(gamepanel,operation);
+                        operation.setGameframe(frame);
+                        Random random = new Random();
+                        int a = random.nextInt(7);
+                        operation.shape=operation.Shape[a];//就这样吧。
+                        frame.setVisible(true);
+                        dispose();
+                    } catch (FileNotFoundException fileNotFoundException) {
+                        fileNotFoundException.printStackTrace();
+                    }
+                }
+            });
+        }
+        JScrollPane scrollPane = new JScrollPane(
+                panel,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS,
+                ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER
+        );
+        setContentPane(scrollPane);
+        setVisible(true);
+
+    }
+
+}
